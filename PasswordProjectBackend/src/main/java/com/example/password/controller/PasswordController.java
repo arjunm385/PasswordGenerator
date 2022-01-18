c3c1fefcfd29a5a7e7bedbe4c54dc60749d2e07e
arjunm385 <arjunm385@gmail.com>
2022-01-18 22:05:16 +0530

    controller added

--- PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
+++ PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
@@ -1,14 +1,29 @@
 package com.example.password.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.password.model.Model;
+import com.example.password.service.Service;
+
 @RestController
+@RequestMapping("/password")
 public class PasswordController {
 
+	@Autowired
+	Service service;
 	
 	@GetMapping("/greet")
 	public String greet() {
 		return "this is greet";
+	}
+	
+	@PostMapping("/generate")
+	public String getRandomPassword(@RequestBody Model model) {
+		return service.getPass(model);
 	}
 }
