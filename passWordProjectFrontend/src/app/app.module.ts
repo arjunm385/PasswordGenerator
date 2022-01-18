52f7ed6a00213941b9f8d9ff482b75dbca211c4e
arjunm385 <arjunm385@gmail.com>
2022-01-19 00:39:23 +0530

    frontend created

---
+++ passWordProjectFrontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MainpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
