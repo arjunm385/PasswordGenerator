a1857db98067cef39b34c601b401e89ed0b1c086
arjunm385 <arjunm385@gmail.com>
2022-01-19 02:46:28 +0530

    Exception file added in backend

--- PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
+++ PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.password.exception.PasswordException;
 import com.example.password.model.Model;
 import com.example.password.model.Response;
 import com.example.password.service.Service;
@@ -20,8 +21,11 @@ public class PasswordController {
 	Service service;
 	
 	@PostMapping("/generate")
-	public Response getRandomPassword(@RequestBody Model model) {
-		System.out.println(model.toString());
-		return service.getPass(model);
+	public Response getRandomPassword(@RequestBody Model model) throws PasswordException {
+		if(model.getDigit()<0 || model.getLowerCase()<0 || model.getSpecialChar()<0 || model.getUpperCase()<0) throw new PasswordException();
+		
+		else if(model.getDigit()>10 || model.getLowerCase()>10 || model.getSpecialChar()>10 || model.getUpperCase()>10) throw new PasswordException();
+		
+		else return service.getPass(model);
 	}
 }
--- PasswordProjectBackend/src/main/java/com/example/password/exception/PasswordException.java
+++ PasswordProjectBackend/src/main/java/com/example/password/exception/PasswordException.java
@@ -1,5 +1,30 @@
 package com.example.password.exception;
 
-public class PasswordException {
+public class PasswordException extends Throwable{
 
+	public PasswordException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PasswordException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
