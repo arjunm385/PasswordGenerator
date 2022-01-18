f6e7e3b827804d0fa2e28676c33a97a21c0a59d7
arjunm385 <arjunm385@gmail.com>
2022-01-19 03:30:18 +0530

    Dockerfile added

--- PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
+++ PasswordProjectBackend/src/main/java/com/example/password/controller/PasswordController.java
@@ -2,6 +2,7 @@ package com.example.password.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,11 @@ public class PasswordController {
 
 	@Autowired
 	Service service;
+	
+	@GetMapping
+	public String greet() {
+		return "hello user";
+	}
 	
 	@PostMapping("/generate")
 	public Response getRandomPassword(@RequestBody Model model) throws PasswordException {
